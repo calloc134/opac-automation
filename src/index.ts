d328cd04829955142ce958962c3a1d331ccee920
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-02-06 02:27:30 +0000

    chore: たぶんほぼ完成

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,15 @@ const main = async () => {
     webhook_url,
   });
 
-  await MessageClient.send(":bell: ジリリリリリリリリ！:bell:");
+  await MessageClient.send("┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯┯");
+  await MessageClient.send("┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷┷");
+  await MessageClient.send(":bell: │ ジリリリリリリリリ！:bell:");
   await MessageClient.send(
-    ":sunrise:   おはようございます。かろ噴水が朝7:30をお知らせします。"
+    ":sunrise: │ おはようございます。かろ噴水が朝7:30をお知らせします。"
   );
-  await MessageClient.send(":books:   貸出図書通知botのお時間です。");
+  await MessageClient.send(":books: │ 貸出図書通知botのお時間です。");
   await MessageClient.send(
-    `本日 ${new Date().toLocaleDateString()} の図書館貸し出し状況をお知らせします`
+    `:bell: │ 本日 ${new Date().toLocaleDateString()} の図書館貸し出し状況をお知らせします`
   );
 
   // 図書館システムにログインする
@@ -27,7 +29,7 @@ const main = async () => {
   if (OpacClientMaybe.isErr()) {
     console.error("[!] エラーが発生しました");
     await MessageClient.send(
-      `:warning: エラーが発生しました: ${OpacClientMaybe.error.statusText}`
+      `:warning: │ エラーが発生しました: ${OpacClientMaybe.error.statusText}`
     );
 
     console.error("[*] レスポンスのテキストを表示します");
@@ -43,7 +45,7 @@ const main = async () => {
   if (lental_list_maybe.isErr()) {
     console.error("[!] エラーが発生しました");
     await MessageClient.send(
-      `:warning: エラーが発生しました: ${lental_list_maybe.error.statusText}`
+      `:warning: │ エラーが発生しました: ${lental_list_maybe.error.statusText}`
     );
     console.error("[*] レスポンスのテキストを表示します");
     console.error(lental_list_maybe.error.statusText);
@@ -55,7 +57,7 @@ const main = async () => {
   const extend_list: string[] = [];
 
   console.log("[*] 現在借りている本の一覧を表示します\n");
-  await MessageClient.send(":books: 現在借りている本の一覧を表示します");
+  await MessageClient.send(":books: │ 現在借りている本の一覧を表示します");
 
   for (const book of lental_list_maybe.value) {
     console.log(
@@ -70,13 +72,13 @@ const main = async () => {
       console.log("[*] この本は延滞しています");
       console.log("[*] 速やかに返却してください");
       await MessageClient.send(
-        ":warning: この本は延滞しています。速やかに返却してください"
+        ":exclamation: │ この本は延滞しています。速やかに返却してください"
       );
     } else if (book.status === "確認") {
       console.log("[*] この本は本日が返却期限です");
       console.log("[*] 延長処理を行います");
       await MessageClient.send(
-        ":warning: この本は本日が返却期限です。延長処理を行います"
+        ":white_check_mark: │ この本は本日が返却期限です。延長処理を行います"
       );
       extend_list.push(book.book_id);
     }
@@ -85,13 +87,13 @@ const main = async () => {
   if (extend_list.length === 0) {
     console.log("[*] 延長する本がありません");
     console.log("[*] プログラムを終了します");
-    await MessageClient.send(":books: 延長する本がありません");
-    await MessageClient.send(":books: プログラムを終了します");
+    await MessageClient.send(":man_gesturing_no: │ 延長する本がありません");
+    await MessageClient.send(":man_gesturing_ok: │ プログラムを終了します");
     return;
   }
 
   console.log("[*] 延長処理を開始します");
-  await MessageClient.send(":books: 延長処理を開始します");
+  await MessageClient.send(":books: │ 延長処理を開始します");
 
   for (const book_id of extend_list) {
     const result = await OpacClient.extend_book({ book_id: book_id });
@@ -100,7 +102,7 @@ const main = async () => {
       console.error("[!] エラーが発生しました");
       console.error("[*] プログラムを終了します");
       await MessageClient.send(
-        ":warning: エラーが発生しました。プログラムを終了します"
+        ":warning: │ エラーが発生しました。プログラムを終了します"
       );
       return;
     }
@@ -108,8 +110,8 @@ const main = async () => {
 
   console.log("[*] 延長処理が完了しました");
   console.log("[*] プログラムを終了します");
-  await MessageClient.send(":books: 延長処理が完了しました");
-  await MessageClient.send(":books: プログラムを終了します");
+  await MessageClient.send(":man_gesturing_ok:│ 延長処理が完了しました");
+  await MessageClient.send(":man_gesturing_ok: │ プログラムを終了します");
 };
 
 main();
